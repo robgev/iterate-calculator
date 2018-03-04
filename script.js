75b3593ba0106ef0f719da93d333b5f8e01ee37d
Robert Gevorgyan <robert1999.g@gmail.com>
2018-03-04 19:02:50 +0400

    Add simple calculator logic

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
-const button1 = document.getElementById("btn-1");
+const calculateTerm = term => {
+  let numbers = [];
+  term.split("*").forEach(elm => {
+    numbers = [ ...numbers, ...elm.split("/")]
+  });
+
+  let result = 1;
+  numbers.forEach(currentNumber => {
+    const splittedNumber = term.split(currentNumber)[0];
+    const sign = splittedNumber[splittedNumber.length - 1];
+    if (sign === '/') {
+      result /= parseInt(currentNumber, 10);
+    } else {
+      result *= parseInt(currentNumber, 10);
+    }
+  })
+
+  return result;
+}
+
+const calculateResult = expression => {
+  let terms = [];
+  expression.split("+").forEach(elm => {
+    terms = [ ...terms, ...elm.split("-")]
+  });
+
+  let result = 0;
+  terms.forEach(currentTerm => {
+    // Split the element, get the first one in the result
+    // because that item keeps the preceding sign
+    const splittedTerm = expression.split(currentTerm)[0];
+    // get the actual sign because of the last element
+    // alternative solution:
+    // const sign = expression.split(currentTerm)[0].pop();
+    const sign = splittedTerm[splittedTerm.length - 1];
+    if (sign === '-') {
+      result -= calculateTerm(currentTerm);
+    } else {
+      result += calculateTerm(currentTerm);
+    }
+  })
+
+  return result;
+}
+
 const textField = document.getElementById("expression-field");
-const buttons = document.querySelectorAll('.button-group button');
+const buttons =
+  document.querySelectorAll('.button-group button:not(.long-button)');
+const equalsButton = document.getElementById("equals");
 buttons.forEach(currentButton => {
   currentButton.addEventListener('click', event => {
     if (textField.value === '0') {
@@ -9,4 +55,8 @@ buttons.forEach(currentButton => {
       textField.value += currentButton.innerText;
     }
   })
+})
+
+equalsButton.addEventListener('click', event => {
+  textField.value = calculateResult(textField.value);
 })
